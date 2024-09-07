16583802e25a426cf077c166406ea7a7c4df3335
vasu <vasu.imandhi@gmail.com>
2024-09-07 13:49:50 +0530

    Initial commit

---
+++ src/main/java/com/vimandi/PlayWithGit/PlayWithGitApplication.java
@@ -0,0 +1,13 @@
+package com.vimandi.PlayWithGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayWithGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayWithGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vimandi/PlayWithGit/PlayWithGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vimandi.PlayWithGit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayWithGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
